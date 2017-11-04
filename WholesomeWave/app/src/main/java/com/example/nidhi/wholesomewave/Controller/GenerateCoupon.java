fdc2e7bb63adaf4d7042caf9addab26bf1b3ed28
SonJoseph <joeson34@cs.unc.edu>
2017-11-04 04:25:30 -0400

    coupon-generate-randomkey

--- WholesomeWave/app/src/main/java/com/example/nidhi/wholesomewave/Controller/GenerateCoupon.java
+++ WholesomeWave/app/src/main/java/com/example/nidhi/wholesomewave/Controller/GenerateCoupon.java
@@ -2,8 +2,11 @@ package com.example.nidhi.wholesomewave.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.nidhi.wholesomewave.R;
+
+import java.util.Random;
 
 public class GenerateCoupon extends AppCompatActivity {
 
@@ -11,5 +14,23 @@ public class GenerateCoupon extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_coupon);
+        String randomKey = getSaltString();
+
+        TextView mTextView = (TextView) findViewById(R.id.Coupon);
+        mTextView.setText(randomKey);
+
+    }
+
+    protected String getSaltString() {
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 18) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
+
     }
 }
