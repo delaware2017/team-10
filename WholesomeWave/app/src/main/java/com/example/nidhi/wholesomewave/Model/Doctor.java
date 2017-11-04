c80b1a28d4a0d9bb87824baba15a7b5180264b25
SrinidhiPalwayi <palwayis25@gmail.com>
2017-11-03 20:37:52 -0400

    model

---
+++ WholesomeWave/app/src/main/java/com/example/nidhi/wholesomewave/Model/Doctor.java
@@ -0,0 +1,8 @@
+package com.example.nidhi.wholesomewave.Model;
+
+/**
+ * Created by nidhi on 11/3/17.
+ */
+
+public class Doctor {
+}
---
+++ WholesomeWave/app/src/main/java/com/example/nidhi/wholesomewave/Model/User.java
@@ -0,0 +1,14 @@
+package com.example.nidhi.wholesomewave.Model;
+
+import android.os.Parcelable;
+
+/**
+ * Created by nidhi on 11/3/17.
+ */
+
+public class User  {
+
+
+
+
+}
