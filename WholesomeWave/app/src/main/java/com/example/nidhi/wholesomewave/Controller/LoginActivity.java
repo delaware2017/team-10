ea67ef2997b18a271f93a04b900d9374cfb2760c
SrinidhiPalwayi <palwayis25@gmail.com>
2017-11-03 23:18:04 -0400

    please work, splash

--- WholesomeWave/app/src/main/java/com/example/nidhi/wholesomewave/Controller/LoginActivity.java
+++ WholesomeWave/app/src/main/java/com/example/nidhi/wholesomewave/Controller/LoginActivity.java
@@ -52,7 +52,7 @@ public class LoginActivity extends AppCompatActivity {
             String password2 = password.getText().toString();
 
 
-            //Intent intent = new Intent(this, MainScreenActivity.class);
+            Intent intent = new Intent(this, MainScreenActivity.class);
             //intent.putExtra("Username", username.getText().toString());
             //startActivity(intent);
         }
